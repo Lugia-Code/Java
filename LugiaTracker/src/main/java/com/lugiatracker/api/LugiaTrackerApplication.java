a72220d70cc34c15a978dc5aa74e5a52adbce80c
jubshereman <lancrazy02@gmail.com>
2025-05-03 14:07:44 -0300

    feat: Secure sensitive information with .env

--- LugiaTracker/src/main/java/com/lugiatracker/api/LugiaTrackerApplication.java
+++ LugiaTracker/src/main/java/com/lugiatracker/api/LugiaTrackerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LugiaTrackerApplication {
 
 	public static void main(String[] args) {
+		com.lugiatracker.config.DotenvConfig.loadEnv();
 		SpringApplication.run(LugiaTrackerApplication.class, args);
 	}
 
