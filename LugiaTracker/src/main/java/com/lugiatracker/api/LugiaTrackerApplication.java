6e4b7c0553e3b2a6e1a23535f63acd9cbafe4948
jubshereman <lancrazy02@gmail.com>
2025-05-03 13:01:10 -0300

    feat:initializing the springboot project

---
+++ LugiaTracker/src/main/java/com/lugiatracker/api/LugiaTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.lugiatracker.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LugiaTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LugiaTrackerApplication.class, args);
+	}
+
+}
---
+++ LugiaTracker/src/test/java/com/lugiatracker/api/LugiaTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lugiatracker.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LugiaTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
