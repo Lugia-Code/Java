a3a81c200e51a41f428a68c4f447c5228c1d6854
jubshereman <lancrazy02@gmail.com>
2025-09-24 18:44:29 -0300

    I KNOW MORE THAN ANYTHING

--- Tracking Code/src/main/java/br/com/fiap/universidade_fiap/model/EnumSetor.java
+++ Tracking Code/src/main/java/br/com/fiap/universidade_fiap/model/EnumSetor.java
@@ -16,8 +16,13 @@ public enum EnumSetor {
         this.descricao = descricao;
     }
 
+    public String getDescricao() {
+        return descricao;
+    }
+
     @Override
     public String toString() {
         return descricao;
     }
 }
+
--- Tracking Code/src/main/java/br/com/fiap/universidade_fiap/model/Setor.java
+++ Tracking Code/src/main/java/br/com/fiap/universidade_fiap/model/Setor.java
@@ -11,40 +11,42 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name="setor")
 public class Setor {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	@Enumerated(EnumType.STRING)
-	private EnumSetor nome;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Enumerated(EnumType.STRING)
+    private EnumSetor nome;
 
+    public Setor() {
+        super();
+    }
 
+    public Setor(Long id, EnumSetor nome) {
+        super();
+        this.id = id;
+        this.nome = nome;
+    }
 
-	public Setor() {
-		super();
-	}
-	
-	
-	public Setor(Long id, EnumSetor nome) {
-		super();
-		this.id = id;
-		this.nome = nome;
-	}
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public EnumSetor getNome() {
-		return nome;
-	}
-	public void setNome(EnumSetor nome) {
-		this.nome = nome;
-	}
-	
+    public EnumSetor getNome() {
+        return nome;
+    }
 
+    public void setNome(EnumSetor nome) {
+        this.nome = nome;
+    }
+
+    // Novo getter para expor a descrição do enum
+    public String getDescricao() {
+        return nome.getDescricao();
+    }
 }
